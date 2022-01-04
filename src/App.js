4bc55f7399bb901f2dedca9b5d3020ab97ab7c2e
Michael Horvilleur <mhorvilleur13@gmail.com>
2022-01-04 16:15:50 -0600

    counter

--- src/App.js
+++ src/App.js
@@ -34,6 +34,26 @@ function TodoForm({ addTodo }) {
   )
 }
 
+/*function TodoCount({ todos }) {
+  const [count, setCount] = useState(0);
+  setCount(Object.keys(todos).length);
+  console.log({ count });
+  return (
+    <div>
+      <h1> {count} </h1>
+    </div>
+  )
+}*/
+
+function TodoCount({ count, addToCount }) {
+  addToCount();
+  return (
+    <div>
+      <h1>{count}</h1>
+    </div>
+  )
+}
+
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -52,9 +72,14 @@ function App() {
 
     }
   ]);
+
+  const [count, setCount] = useState(3);
+
+
   useEffect(() => {
     console.log('On Mount')
   }, []);
+
   const addTodo = text => {
     const newTodos = [...todos, { text }];
     setTodos(newTodos);
@@ -69,6 +94,12 @@ function App() {
     newTodos.splice(index, 1);
     setTodos(newTodos);
   }
+
+  const addToCount = () => {
+    setCount(Object.keys(todos).length);
+  }
+
+
   return (
     <div className="app">
       <div className="todo-list">
@@ -79,6 +110,7 @@ function App() {
             removeTodo={removeTodo} />
         ))}
         <TodoForm addTodo={addTodo} />
+        <TodoCount count={count} addToCount={addToCount} />
       </div>
 
     </div>
